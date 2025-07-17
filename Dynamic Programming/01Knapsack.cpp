c7d6b2af35844a59753103b22ac7d2db8e686948
malhardv <malhar.vhatkar@gmail.com>
2025-07-17 12:16:37 +0530

    dp

--- Dynamic Programming/01Knapsack.cpp
+++ Dynamic Programming/01Knapsack.cpp
@@ -84,24 +84,54 @@ int solveUsingTab(int capacity, vector<int> weight, vector<int> profit, int inde
             }
             int exclude = dp[i][j + 1];
 
-            dp[i][j] = max(include, exclude); 
+            dp[i][j] = max(include, exclude);
         }
     }
 
     return dp[capacity][0];
 }
 
+int solveUsingSO(int capacity, vector<int> weight, vector<int> profit, int n)
+{
+    vector<int> next(capacity + 1, 0);
+    // vector<int> curr(capacity + 1, -1);
+
+    // for (int row = 0; row <= capacity; row++)
+    // {
+    //     next[row] = 0;
+    // }
+
+    for (int j = n - 1; j >= 0; j--)
+    {
+        for (int i = capacity; i >= 0; i--)
+        {
+            int include = 0;
+            if (weight[j] <= i)
+            {
+                include = profit[j] + next[i - weight[j]];
+            }
+            int exclude = next[i]; 
+            next[i] = max(include, exclude);
+        }
+
+        // // shifting the arrays from right to left
+        // next = curr;
+    }
+
+    return next[capacity];
+}
+
 int main()
 {
 
-    int capacity = 50;
-    vector<int> weight = {10, 20, 30};
-    vector<int> profit = {60, 100, 120};
+    int capacity = 6;
+    vector<int> weight = {1, 2, 3};
+    vector<int> profit = {10, 15, 40};
     int index = 0;
     int n = 3;
     vector<vector<int>> dp(capacity + 1, vector<int>(n + 1, -1));
 
-    int ans = solveUsingTab(capacity, weight, profit, index, n);
+    int ans = solveUsingSO(capacity, weight, profit, n);
     cout << "Max Profit: " << ans;
 
     return 0;
