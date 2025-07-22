f7ac137f88cb80bd76dfcf4990e66256b69c923c
malhardv <malhar.vhatkar@gmail.com>
2025-07-23 00:41:51 +0530

    1

--- Graphs/implementation.cpp
+++ Graphs/implementation.cpp
@@ -3,10 +3,11 @@
 #include<unordered_map>
 using namespace std;
 
-class Graph {
+template <typename T>
+class UGraph {       // Unweighted Graph
     public:
-    unordered_map<int, list<int>> adjList;
-    void addEdge(int u, int v, bool direction) {
+    unordered_map<T, list<T>> adjList;
+    void addEdge(T u, T v, bool direction) {
         // direction -> 0 -> undirected
         // direction -> 1 -> directed
         if(direction == 1) {
@@ -18,12 +19,12 @@ class Graph {
             adjList[u].push_back(v);
             adjList[v].push_back(u);
         }
-
+        
         cout<<"Printing Adjacency List: "<<endl;
         printAdjList();
         cout<<endl;
     }
-
+    
     void printAdjList() {
         for(auto i: adjList) {
             cout<< i.first << " -> { ";
@@ -35,12 +36,45 @@ class Graph {
     }
 };
 
+template <typename T>
+class WGraph {        // Weighted Graph
+    public: 
+    unordered_map<T, list<pair<T, T>>> adjList;
+    
+    void addEdge(T u, T v, bool direction, T weight) {
+        
+        // if directed edge
+        if(direction == 1) {
+            adjList[u].push_back({v, weight});
+        } else {    // undirected edge
+            // direction == 0
+            adjList[u].push_back({v, weight});
+            adjList[v].push_back({u, weight});
+        }
+
+        
+        cout<<"Printing Adjacency List: "<<endl;
+        printAdjList();
+        cout<<endl;
+    }
+    
+    void printAdjList() {
+        for(auto i: adjList) {
+            cout<< i.first << " -> { ";
+            for(auto neighbour: i.second) {
+                cout<<" { Node: "<<neighbour.first <<"  Weight: "<<neighbour.second<<" }";
+            }
+            cout<<" }"<<endl;
+        }
+    }
+};
+
 int main() {
-    Graph g;
-    g.addEdge(0, 1, 0);
-    g.addEdge(1, 2, 0);
-    g.addEdge(1, 3, 0);
-    g.addEdge(2, 3, 0);
+    WGraph<int> g;                    // using template we can define the specific datatype during declaration
+    g.addEdge(0, 1, 1, 20);
+    g.addEdge(1, 2, 1, 30);
+    g.addEdge(1, 3, 1, 50);
+    g.addEdge(2, 3, 1, 40);
 
     return 0;
 }
